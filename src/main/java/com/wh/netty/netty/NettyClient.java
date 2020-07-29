b753d757dc6f6f5746a4cfd568927d76c58b763e
王辉 <wangh_0929@163.com>
2020-07-29 09:04:43 +0800

    netty客户端

---
+++ src/main/java/com/wh/netty/netty/NettyClient.java
@@ -0,0 +1,43 @@
+package com.wh.netty.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author wh
+ * @date 2020/7/29
+ * Description: netty客户端
+ */
+public class NettyClient {
+    public static void main(String[] args) {
+        //客户端需要一个时间循环组
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyClientHandle());
+                        }
+                    });
+            System.out.println("客户端 ok...");
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+
+    }
+
+}
---
+++ src/main/java/com/wh/netty/netty/NettyClientHandle.java
@@ -0,0 +1,41 @@
+package com.wh.netty.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author wh
+ * @date 2020/7/29
+ * Description: netty客户端处理类
+ */
+public class NettyClientHandle extends ChannelInboundHandlerAdapter {
+    /**
+     * 当通道就绪时，就会触发此方法
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client ctx:" + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello， server", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 当通道有读取数据是触发此方法
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        Channel channel = ctx.channel();
+        System.out.println("服务端发送的消息是：" + byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务端地址是：" + channel.remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
